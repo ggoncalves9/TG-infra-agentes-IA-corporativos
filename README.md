ff2f49e0e12e465852a55f76a2eed685dc8f9023
GuilhermeProz <guilherme.milesi@proz...>
2025-06-07 15:06:22 -0300

    Comentado copia de .env e add docker compose

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Agentes Corporativos
  Arq. par Infra de Agentes de Inteligencia Artificial para Corporações.
+
+
+Criar VM linux
+
+Clonar o repo do github
+
+executar chmod +x no script_install.sh
+
+executar ./script_install.sh
+
+
--- script_install.sh
+++ script_install.sh
@@ -5,15 +5,19 @@ echo ">>> Atualizando sistema..."
 sudo apt update && sudo apt upgrade -y
 
 echo ">>> Instalando Docker Engine + Compose..."
-curl -fsSL https://get.docker.com | sudo bash
+curl -fsSL https://get.docker.com | sudo bash -s -- -y
 sudo usermod -aG docker "$USER"
+
+echo ">>> Instalando Docker Compose Plugin..."
+sudo apt-get update -y
+sudo apt-get install -y docker-compose-plugin
 
 # echo ">>> Clonando repositório..."
 # git clone https://github.com/seu-usuario/ai-agents-mvp.git
 # cd ai-agents-mvp
 
-echo ">>> Copiando .env e subindo stack..."
-cp .env.sample .env
-docker compose up -d --build
+# echo ">>> Copiando .env e subindo stack..."
+# cp .env.sample .env
+# docker compose up -d --build
 
 echo ">>> Pronto! Acesse http://localhost:5678"
